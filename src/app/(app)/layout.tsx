392f829ea1203eb41d026bbe3fdcddf3e52ba41a
Hari Prasanth <ryomen.143.sukuna@gmail.com>
2025-12-26 02:56:14 +0000

    Align centre and increase font weight  (_for element h1_)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,10 @@
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <header className="sticky top-0 flex h-16 items-center justify-center gap-4 border-b bg-background px-4 md:px-6">
         <div className="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-primary h-6 w-6"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M12 2a10 10 0 0 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/></svg>
-            <h1 className="text-xl font-semibold tracking-tighter">Pan cake 🎂</h1>
+            <h1 className="text-2xl font-bold tracking-tighter">Pan cake 🎂</h1>
         </div>
       </header>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
